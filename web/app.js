75d15997102c728491133b8a4e269a09fb7991cd
James Van Mil <james.vanmil@gmail.com>
2014-12-20 19:45:30 +0000

    form for editing name in search query

--- web/app.js
+++ web/app.js
@@ -5,7 +5,38 @@
     var search = this;
     search.results = [ ];
 
-    $http.get('http://thesubdirectory.com:8983/solr/refuge/browse?q=*%3A*&rows=1000&wt=json&indent=true').success(function(data){
+    // defaults gather all of the objects
+    this.queryDefaults = {
+      q: '*',
+      rows: '100',
+      wt: 'json',
+    };
+
+    this.query = search.queryDefaults;
+    
+
+    // construct the search URL
+    function buildSolrQuery(params) {
+      var baseUrl = "http://thesubdirectory.com:8983/solr/refuge/browse?";
+      var paramArray = Object.keys(params);
+      var query = "";
+      for (var i = 0, len = paramArray.length; i < len; i++) {
+        key = paramArray[i];
+        value = params[key];
+        query = query + key + "=" + value +"&";
+      }
+      return baseUrl + query;
+    };
+
+    // get new results
+    this.updateQuery = function() {
+      $http.get(buildSolrQuery(this.query)).success(function(data){
+        search.results = data;
+      });
+    };
+
+    // default results - will load when initialized
+    $http.get(buildSolrQuery(this.query)).success(function(data){
       search.results = data;
     });
   }]);
